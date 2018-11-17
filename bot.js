0e0f99aacea358f22363aed3fca32c9e6fc06754
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-17 22:46:52 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -61,4 +61,67 @@ client.on('message', message => {
   });
 
 
+client.on('message', message => {
+	if (message.channel.type == "dm") return;
+	if (message.member.hasPermission('ADMINISTRATOR')) return; // This ignores admins and lets them say bad words. If you don't want admins saying bad words, delete this line.
+    const swearWords = ["قود","نيك","كس امك"]; // These are the words that wll be filtered. If you would like to add more, simple add ,"word" inbetween the [ and ] and then it'll filter that word as well
+    if (swearWords.some(word => message.content.toLowerCase().includes(word))) {
+        message.delete();
+        message.channel.send(`Hey ${message.author}! That word has been banned, please don't use it!`).then(m => m.delete(3000)); // This function will tell the user off for using the filtered words, and then the message which telsl the user off will be deleted after 3 seconds. If you would like to extend the time, feel free to change it but take note that it's measured in milliseconds. If you don't want the bot to remove the warning message, take off the ".then(m => m.delete(3000))" bit!
+        embed = new Discord.RichEmbed() // The log feature will log embeds, instead of simple messages. This improves the look of the word filter and makes it easier to code.
+        embed.setAuthor(name=`${message.author.tag}`, icon=message.author.avatarURL) // The author label will show the user who actually used the word. It will show their FULL tag and their profile picture.
+        embed.setDescription('Offensive or curse word found in message, in '+ message.channel) // This will tell you which channel the word was used in.
+        embed.setColor(0xff0000) // This is just a random colour. If you'd like to change it, simple change the "ff0000" to a different code. Make sure "0x" stays before the number.
+        embed.addField(name="Message:", value=message.content) // This will tell you the entire message, so you can spot out the word which was used.
+        embed.setFooter(name=`ID: ${message.author.id}`) // This will give you the UserID of the user who used a filtered word in the embed's footer.
+        embed.setTimestamp() // This will tell you what time the word was used at.
+
+        guild = client.guilds.get("511577932544737281")
+        channel = guild.channels.find("id", "511676663982063637") // This will find the channel which it will send the log embed into.
+        channel.send(embed)
+      }
+});
+
+client.on('message', message => {
+	if (message.channel.type == "dm") return;
+	if (message.member.hasPermission('ADMINISTRATOR')) return; // This ignores admins and lets them say bad words. If you don't want admins saying bad words, delete this line.
+    const swearWords = ["نصب","ينعل امك","يبن لقحبه"]; // These are the words that wll be filtered. If you would like to add more, simple add ,"word" inbetween the [ and ] and then it'll filter that word as well
+    if (swearWords.some(word => message.content.toLowerCase().includes(word))) {
+        message.delete();
+        message.channel.send(`Hey ${message.author}! That word has been banned, please don't use it!`).then(m => m.delete(3000)); // This function will tell the user off for using the filtered words, and then the message which telsl the user off will be deleted after 3 seconds. If you would like to extend the time, feel free to change it but take note that it's measured in milliseconds. If you don't want the bot to remove the warning message, take off the ".then(m => m.delete(3000))" bit!
+        embed = new Discord.RichEmbed() // The log feature will log embeds, instead of simple messages. This improves the look of the word filter and makes it easier to code.
+        embed.setAuthor(name=`${message.author.tag}`, icon=message.author.avatarURL) // The author label will show the user who actually used the word. It will show their FULL tag and their profile picture.
+        embed.setDescription('Offensive or curse word found in message, in '+ message.channel) // This will tell you which channel the word was used in.
+        embed.setColor(0xff0000) // This is just a random colour. If you'd like to change it, simple change the "ff0000" to a different code. Make sure "0x" stays before the number.
+        embed.addField(name="Message:", value=message.content) // This will tell you the entire message, so you can spot out the word which was used.
+        embed.setFooter(name=`ID: ${message.author.id}`) // This will give you the UserID of the user who used a filtered word in the embed's footer.
+        embed.setTimestamp() // This will tell you what time the word was used at.
+
+        guild = client.guilds.get("511577932544737281")
+        channel = guild.channels.find("id", "511676663982063637") // This will find the channel which it will send the log embed into.
+        channel.send(embed)
+      }
+});
+
+client.on('message', message => {
+	if (message.channel.type == "dm") return;
+	if (message.member.hasPermission('ADMINISTRATOR')) return; // This ignores admins and lets them say bad words. If you don't want admins saying bad words, delete this line.
+    const swearWords = ["نصبو","ينصبو","نصابين"]; // These are the words that wll be filtered. If you would like to add more, simple add ,"word" inbetween the [ and ] and then it'll filter that word as well
+    if (swearWords.some(word => message.content.toLowerCase().includes(word))) {
+        message.delete();
+        message.channel.send(`Hey ${message.author}! That word has been banned, please don't use it!`).then(m => m.delete(3000)); // This function will tell the user off for using the filtered words, and then the message which telsl the user off will be deleted after 3 seconds. If you would like to extend the time, feel free to change it but take note that it's measured in milliseconds. If you don't want the bot to remove the warning message, take off the ".then(m => m.delete(3000))" bit!
+        embed = new Discord.RichEmbed() // The log feature will log embeds, instead of simple messages. This improves the look of the word filter and makes it easier to code.
+        embed.setAuthor(name=`${message.author.tag}`, icon=message.author.avatarURL) // The author label will show the user who actually used the word. It will show their FULL tag and their profile picture.
+        embed.setDescription('Offensive or curse word found in message, in '+ message.channel) // This will tell you which channel the word was used in.
+        embed.setColor(0xff0000) // This is just a random colour. If you'd like to change it, simple change the "ff0000" to a different code. Make sure "0x" stays before the number.
+        embed.addField(name="Message:", value=message.content) // This will tell you the entire message, so you can spot out the word which was used.
+        embed.setFooter(name=`ID: ${message.author.id}`) // This will give you the UserID of the user who used a filtered word in the embed's footer.
+        embed.setTimestamp() // This will tell you what time the word was used at.
+
+        guild = client.guilds.get("511577932544737281")
+        channel = guild.channels.find("id", "511676663982063637") // This will find the channel which it will send the log embed into.
+        channel.send(embed)
+      }
+});
+
 client.login(process.env.BOT_TOKEN);
